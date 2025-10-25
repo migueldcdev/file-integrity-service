059378c7212ec5daecf276f4623e8ca65267b21b
migueldcdev <migueldc.dev@protonmail.com>
2025-10-25 13:05:36 +0200

    #added basic db ops

--- internal/db/file.go
+++ internal/db/file.go
@@ -23,3 +23,40 @@ func SaveFileHash(hf HashedFile) error {
 
 	return nil
 }
+
+func GetAllHashedFiles() ([]HashedFile, error) {
+	var err error
+	rows, err := DB.Query("SELECT id, path, hash, size, created_at, last_checked FROM hash")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var hashedFiles []HashedFile
+
+	for rows.Next() {
+		var hf HashedFile
+		err := rows.Scan(&hf.Id, &hf.Path, &hf.Hash, &hf.Size, &hf.Created_at, &hf.Last_checked)
+		if err != nil {
+			return nil, err
+		}
+
+		hashedFiles = append(hashedFiles, hf)
+
+	}
+
+	return hashedFiles, nil
+}
+
+func UpdateFileHash(hfId int, hash string) error {
+	var err error
+	_, err = DB.Exec("UPDATE hash SET hash=$1, created_at=$2 WHERE id=$3", hash, hfId, time.Now())
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
