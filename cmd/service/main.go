11e1e24da1646e83decc06946d610e98b1529761
migueldcdev <migueldc.dev@protonmail.com>
2025-10-25 22:17:58 +0200

    #done refactor main func

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/migueldcdev/file-integrity-service/internal/monitor"
 	"github.com/migueldcdev/file-integrity-service/internal/service"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 const DB_PATH string = "../file-integrity-service/data/sqlite.db"
@@ -22,12 +24,17 @@ func main() {
 		fmt.Fprintf(os.Stderr, "An error occurred while initializing DB: %v\n", err)
 		os.Exit(1)
 	}
+
 	path := flag.String("path", "", "dir path to watch")
 	flag.Parse()
 
 	go api.RunAPIServer()
+	go monitor.WatchDirectory(*path)
 	service.WalkDirAndHashFiles(*path)
 
-	monitor.WatchDirectory(*path)
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	<-sigChan
 
+	fmt.Println("\nService terminated")
 }
