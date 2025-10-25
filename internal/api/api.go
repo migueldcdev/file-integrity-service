16a81a8e1b0e7fd7190c899e4dcf2c10ac25f8de
migueldcdev <migueldc.dev@protonmail.com>
2025-10-25 13:52:25 +0200

    #done get hashed files route

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,18 +1,32 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/migueldcdev/file-integrity-service/internal/db"
 	"net/http"
 )
 
 func getHashedFiles(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, "hi\n")
+	hashedFiles, err := db.GetAllHashedFiles()
+
+	if err != nil {
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(hashedFiles); err != nil {
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+	}
 }
 
 func RunAPIServer() {
 
 	fmt.Println("File Integrity API server running on localhost:8090")
-	http.HandleFunc("/", getHashedFiles)
+	http.HandleFunc("/hashedfiles", getHashedFiles)
 	err := http.ListenAndServe(":8090", nil)
 	if err != nil {
 		fmt.Printf("API server error: %v\n", err)
