86f6146a6b8155367e256d7be71252446167a9f4
migueldcdev <migueldc.dev@protonmail.com>
2025-11-06 21:59:04 +0100

    done add api check file integrity & update file hash

--- internal/api/api.go
+++ internal/api/api.go
@@ -4,8 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/migueldcdev/file-integrity-service/internal/db"
+	"github.com/migueldcdev/file-integrity-service/internal/hash"
 	"net/http"
 )
+
+type UpdateFileRequest struct {
+	Path string `json:"path"`
+}
 
 func getHashedFiles(w http.ResponseWriter, req *http.Request) {
 	hashedFiles, err := db.GetAllHashedFiles()
@@ -23,11 +28,78 @@ func getHashedFiles(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func RunAPIServer() {
+func checkFileIntegrity(w http.ResponseWriter, req *http.Request) {
+	path := req.URL.Query().Get("path")
 
+	if path == "" {
+		badRequestError := http.StatusBadRequest
+		http.Error(w, "The path to the file is required.", badRequestError)
+	}
+
+	h, err := db.GetFileHashByPath(path)
+
+	if err != nil {
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+	}
+
+	ch, err := hash.ComputeFileHash(path)
+
+	if err != nil {
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+	}
+
+	if h == ch {
+		fmt.Fprintln(w, "The file is intact and verified.")
+	} else {
+		fmt.Fprintln(w, "Integrity check failed for file. Possible tampering detected!")
+	}
+}
+
+func updateFileHash(w http.ResponseWriter, req *http.Request) {
+	var reqBody UpdateFileRequest
+	if err := json.NewDecoder(req.Body).Decode(&reqBody); err != nil {
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+
+	path := reqBody.Path
+
+	if path == "" {
+		badRequestError := http.StatusBadRequest
+		http.Error(w, "The path to the file is required.", badRequestError)
+		return
+	}
+
+	h, err := hash.ComputeFileHash(path)
+
+	if err != nil {
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+		return
+	}
+
+	err = db.UpdateFileHash(path, h)
+
+	if err != nil {
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+		return
+	}
+
+	fmt.Fprintln(w, "File hash succesfuly updated")
+}
+
+func RunAPIServer() {
 	fmt.Println("File Integrity API server running on localhost:8090")
+
 	http.HandleFunc("/hashedfiles", getHashedFiles)
+	http.HandleFunc("/check-file-integrity", checkFileIntegrity)
+	http.HandleFunc("/update-file-hash", updateFileHash)
+
 	err := http.ListenAndServe(":8090", nil)
+
 	if err != nil {
 		fmt.Printf("API server error: %v\n", err)
 	}
